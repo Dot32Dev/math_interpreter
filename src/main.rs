07acfd3b5d905f7b3d9369c7ef2d50859fefc860
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2024-03-27 17:04:43 +0800

    Some cleanup

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ enum Token {
 
 #[derive(Debug)]
 struct SyntaxError {
+    // My code doesn't access this field, only Rust does when the main function returns an error.
+    #[allow(unused)]
     message: String,
 }
 
@@ -25,10 +27,10 @@ impl SyntaxError {
 }
 
 fn main() -> Result<(), SyntaxError> {
-    let tokens = lexer("2 * 3 * 4 * 5").unwrap();
+    let tokens = lexer("2 * 3 * 4 * 5")?;
     println!("{:?}", tokens);
 
-    parse(&tokens);
+    parse(&tokens)?;
 
     let mut answer = 0.0;
     let mut last_operand = Token::Add;
@@ -149,34 +151,16 @@ fn parse(input: &[Token]) -> Result<(), SyntaxError> {
                         })
                     }))
                     .collect();
-                // let left_factor = term[0].to_number()?;
-                // let operator = term[1].to_operator()?;
-                // let right_factor = term[2].to_number()?;
-                // let node = Node::BinaryOp {
-                //     left: Box::new(Node::Number(left_factor)),
-                //     op: operator,
-                //     right: Box::new(Node::Number(right_factor)),
-                // };
-                let final_node = match term.len() {
+
+                let term_node = match term.len() {
                     n if n % 2 == 1 && n > 1 => {
                         // Only matches if the length is 3, 5, 7, etc (makes room for operators between factors)
-                        // let left_factor = term[0].to_number()?;
-                        // let operator = term[1].to_operator()?;
-                        // let right_factor = term[2].to_number()?;
-                        // let node = Node::BinaryOp {
-                        //     left: Box::new(Node::Number(left_factor)),
-                        //     op: operator,
-                        //     right: Box::new(Node::Number(right_factor)),
-                        // };
 
-                        // let node = Node::BinaryOp {
-                        //     left: Box::new(node),
-                        //     op: term[3].to_operator()?,
-                        //     right: Box::new(Node::Number(term[4].to_number()?)),
-                        // };
+                        let left_factor = term[0].to_number()?;
+                        let mut node = Node::Number(left_factor);
 
-                        let mut node = Node::Number(term[0].to_number()?);
-
+                        // Iterates two places at a time
+                        // for [item_one, item_two] in my_vector.iter().skip(1).chunks(2) {
                         for i in (1..term.len()).step_by(2) {
                             let operator = term[i].to_operator()?;
                             let right_factor = term[i + 1].to_number()?;
@@ -198,7 +182,7 @@ fn parse(input: &[Token]) -> Result<(), SyntaxError> {
                         return Err(SyntaxError::new("Malformed expression".to_string()));
                     }
                 };
-                println!("{:#?}", final_node);
+                println!("{:#?}", term_node);
                 println!("{:?}", term);
             }
             _ => (),
